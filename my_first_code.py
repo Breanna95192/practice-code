59acaf8d55bf33fa09d3db49510b8d0f28391cb7
Breanna Mbuyambo Mbuyambo (s5712981) <s5712981@bournemouth.ac.uk>
2024-12-14 17:48:17 +0000

    Initial Message

---
+++ my_first_code.py
@@ -0,0 +1,25 @@
+name = "Breanna"
+print("Hello(Breanna)")
+
+#Performmig calculations in Python using (+, - , *, %, //, **)
+print(265+5924)
+print(5022-901)
+print(5*10)
+print(604/4)
+print(3%2)
+print(10%4)
+print(9//3)
+print(8//3)
+print(5**2)
+print(4**3)
+
+#Another calculation using variable assignments 
+
+a = 6
+b = 4
+c = 5
+d = 2 
+e = 4
+f = 3
+answer = (a*b) + (c**d) - (e%f)
+print(answer)
